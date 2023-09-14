efaa7fd95c61cece522ba7b8d11ce5d8a95e2e65
yangshengqin <18513100108@163.com>
2023-09-14 19:46:18 +0800

    ai_translator 增加私有化模型chatglm调用

--- project/langchain_openai_translator/ai_translator/translator/translation_chain.py
+++ project/langchain_openai_translator/ai_translator/translator/translation_chain.py
@@ -1,5 +1,6 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
+from langchain.llms import ChatGLM
 
 from langchain.prompts.chat import (
     ChatPromptTemplate,
@@ -11,7 +12,7 @@ from utils import LOG
 
 class TranslationChain:
     def __init__(self, model_name: str = "gpt-3.5-turbo", verbose: bool = True):
-        
+
         # 翻译任务指令始终由 System 角色承担
         template = (
             "You are a translation expert, proficient in various languages, \n"
@@ -28,9 +29,14 @@ class TranslationChain:
         chat_prompt_template = ChatPromptTemplate.from_messages(
             [system_message_prompt, human_message_prompt]
         )
-
-        # 为了翻译结果的稳定性，将 temperature 设置为 0
-        chat = ChatOpenAI(model_name=model_name, temperature=0, verbose=verbose)
+        if model_name == "gpt-3.5-turbo":
+            # 为了翻译结果的稳定性，将 temperature 设置为 0
+            chat = ChatOpenAI(model_name=model_name, temperature=0, verbose=verbose)
+        elif model_name == "chat_glm":
+            endpoint_url = ("http://127.0.0.1:8000")  # endpoint_url 填写跑模型的地址
+            chat = ChatGLM(endpoint_url=endpoint_url, temperature=0, verbose=verbose)
+        else:
+            raise Exception(f"This model is not supported. ModelName:{model_name}")
 
         self.chain = LLMChain(llm=chat, prompt=chat_prompt_template, verbose=verbose)
 
